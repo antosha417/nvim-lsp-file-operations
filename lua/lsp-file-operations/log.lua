8ac757e13707b070d3514360a69e78d2f24e9206
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-09-24 03:46:45 -0600

    fix: log output to logfile specified in `README.md`
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/lsp-file-operations/log.lua
+++ lua/lsp-file-operations/log.lua
@@ -6,6 +6,57 @@ local levels = {
   warn = vim.log.levels.WARN,
 }
 
+local output = vim.fs.joinpath(vim.fn.stdpath("cache"), "lsp-file-operations.log")
+
+---@param mode string
+---@return integer fmode
+local function fmode(mode)
+  return tonumber(mode, 8)
+end
+
+---@param msg string
+local function write_output(msg)
+  require("lsp-file-operations.utils").validate({ msg = { msg, { "string" } } })
+
+  local uv = vim.uv or vim.loop
+  local stat = uv.fs_stat(output)
+  if stat and stat.type == "directory" then
+    return
+  end
+
+  local fd ---@type integer|nil|?
+  if not stat then
+    fd = uv.fs_open(output, "w", fmode("644"))
+    if not fd then
+      return
+    end
+    uv.fs_close(fd)
+  end
+
+  fd = uv.fs_open(output, "r", fmode("644"))
+  if not fd then
+    return
+  end
+
+  stat = uv.fs_stat(output)
+  if not stat then
+    uv.fs_close(fd)
+    return
+  end
+
+  local data = uv.fs_read(fd, stat.size)
+  uv.fs_close(fd)
+  if not data then
+    return
+  end
+
+  fd = uv.fs_open(output, "w", fmode("664"))
+  if fd then
+    uv.fs_write(fd, ("%s%s%s"):format(data, data ~= "" and "\n" or "", msg))
+    uv.fs_close(fd)
+  end
+end
+
 ---@class LspFileOps.Log
 ---@field level "debug"|"info"|"error"|"warn"
 local M = {}
@@ -37,10 +88,19 @@ local function gen_log_func(level)
 
       if msg ~= "" then -- NOTE: Avoid notifying on empty output
         vim.schedule(function() -- HACK: Use `vim.schedule` to avoid mangling the output of tests
-          vim.notify(
-            ("nvim-lsp-file-operations [%s]: %s"):format(level:upper(), msg),
-            levels[level]
+          local date = os.date("*t") --[[@as osdate]]
+          local txt = ("[nvim-lsp-file-operations] [%s %d-%d-%d %02d:%02d:%02d]: %s"):format(
+            level:upper(),
+            date.year,
+            date.month,
+            date.day,
+            date.hour,
+            date.min,
+            date.sec,
+            msg
           )
+          vim.notify(txt, levels[level])
+          write_output(txt)
         end)
       end
     end
